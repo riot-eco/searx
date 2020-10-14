4f70f66b89b6e755b0f7cf07ec6f07a8c5d56b71
Mischa Buchhofer <mischa.buchhofer@stud.h-da.de>
2020-10-14 23:23:30 +0200

    add bing_api engine

---
+++ searx/engines/bing_api.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import requests
+import urllib.parse
+
+categories = ['general']
+
+def request(query, params):
+    '''pre-request callback
+    params<dict>:
+      method  : POST/GET
+      headers : {}
+      data    : {} # if method == POST
+      url     : ''
+      category: 'search category'
+      pageno  : 1 # number of the requested page
+    '''
+
+    paging = True
+    language_support = True
+
+    # Add your Bing Search V7 subscription key and endpoint to your environment variables.
+    count_wish = 50
+    market = params['language'] #"de-DE"
+    offset = params['pageno']
+    page_no = offset * count_wish
+    safesearch = "Moderate"
+    subscription_key = os.environ['BING_SEARCH_V7_SUBSCRIPTION_KEY']
+    endpoint = os.environ['BING_API_ENDPOINT'] + "/bing/v7.0/search" + "?count=" + str(
+        count_wish) + "&mkt=" + market + "&safesearch=" + safesearch
+
+    params['url'] = endpoint + "q=" + urllib.parse.quote(query)
+    params['headers']['Ocp-Apim-Subscription-Key'] = subscription_key
+
+    return params
+
+
+
+def response(resp):
+    '''post-response callback
+    resp: requests response object
+    '''
+    resp = response.json()
+    results = []
+
+    for p in resp["webPages"]["value"]:
+        res = {'url': p.url, 'title': p.name, 'content': p.snippet}
+        results.append(res)
+
+    return results
+
